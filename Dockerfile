b1498dd4bd66d9942c2a65a163d7cbe45b035d93
Victor Hugo <victorblog410@gmail.com>
2021-06-22 18:02:03 -0300

    added PostgreSQL integration into docker-compose

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# pull oficial base image
+FROM python:3.9.1-slim-buster
+
+# set workinng directory
+
+# set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+
+ADD . /code
+WORKDIR /code
+
+COPY poetry.lock pyproject.toml /tmp/
+
+
+RUN pip install poetry
+RUN cd /tmp && poetry export -f requirements.txt --output /tmp/requirements.txt
+RUN pip install -r /tmp/requirements.txt
+
+
+# copy entrypoint.sh
+COPY entrypoint.sh /usr/src/app/entrypoint.sh
+RUN chmod +x /usr/src/app/entrypoint.sh
+
+# run entrypoint.sh
+ENTRYPOINT ["/usr/src/app/entrypoint.sh"]
--- drf_study/settings.py
+++ drf_study/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=$1bwn=t)w@01l*k7hv1jjf8c0-_%r=&$e-xrjaz&2_sb9n-xe'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -75,10 +77,17 @@ WSGI_APPLICATION = 'drf_study.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(
+            BASE_DIR, "db.sqlite3")
+                               ),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
