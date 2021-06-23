c1f73aedc4753c6c1213f07cb639b2e27a25e872
Victor Hugo <victorblog410@gmail.com>
2021-06-22 22:17:12 -0300

    added test invalid post and invalid keys

--- drf_study/study/tests/test_views.py
+++ drf_study/study/tests/test_views.py
@@ -23,3 +23,49 @@ def test_add_movie(client):
     assert resp.data["title"] == "Pythonico"
     study = Study.objects.all()
     assert len(study) == 1
+
+
+@pytest.mark.django_db
+def test_add_invalid_json(client):
+    study = Study.objects.all()
+    assert len(study) == 0
+
+    resp = client.post(
+        "/api/study/",
+        {},
+        content_type="application/json"
+    )
+    assert resp.status_code == 400
+
+    study = Study.objects.all()
+    assert len(study) == 0
+
+
+@pytest.mark.django_db
+def test_add_invalid_json_keys(client):
+    study = Study.objects.all()
+    assert len(study) == 0
+
+    resp = client.post(
+        "/api/study/",
+        {
+            "title": "Python",
+            "category": "loop"
+            },
+        content_type="application/json"
+    )
+    assert resp.status_code == 400
+
+    study = Study.objects.all()
+    assert len(study) == 0
+
+
+def test_get_single_study(client):
+    study = Study.objects.create(
+        title='Docker',
+        category='Virtualization',
+        description="comandos mais utilizados"
+    )
+    resp = client.get(f"/api/study/{study.id}/")
+    assert resp.status_code == 200
+    assert resp.data["title"] == "Docker"
